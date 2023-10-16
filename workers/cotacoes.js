196afbb8960a456a9181b8828b374597d4063f5b
Felipe Rogerio <felipe.rogerio@outlook.com>
2023-10-16 14:07:07 -0300

    add: Tratativa de erros e retentativas

--- workers/cotacoes.js
+++ workers/cotacoes.js
@@ -4,18 +4,25 @@ const { buscaCotacoesOnline } = require('../services');
 const { logger } = require('../utils');
 
 
-const cotacoesWorker = async (_job, done) => {
-    logger.info('Buscando cotações...');
-
-    const cotacoes = await buscaCotacoesOnline();
-
-    logger.info('Cotações requisitadas com sucesso...');
-
-    await Cotacao.insertMany(cotacoes);
-
-    logger.info('Cotações inseridas no banco!')
-
-    done();
+const cotacoesWorker = async (job, done) => {
+    try {
+        console.log("chegou aqui")
+        logger.info(`Buscando cotações... Tentativa ${job.attemptsMade + 1}/${job.opts.attempts}`);
+    
+        const cotacoes = await buscaCotacoesOnline();
+    
+        logger.info('Cotações requisitadas com sucesso...');
+    
+        await Cotacao.insertMany(cotacoes);
+    
+        logger.info('Cotações inseridas no banco!')
+    
+        done();
+    }
+    catch (err) {
+        logger.error(`Erro ao processar o job ${err.message}`);
+        done(err);
+    }
 
 };
 
--- workers/index.js
+++ workers/index.js
@@ -6,8 +6,19 @@ const cotacoesQueue = new Queue('busca-cotacoes', process.env.REDIS_URL);
 
 cotacoesQueue.process(cotacoesWorker);
 
-const agendaTarefas = () => {
-    cotacoesQueue.add({}, {repeat: { cron: '0/15 * * * *' }});
+const agendaTarefas = async () => {
+    const cotacoesAgendadas = await cotacoesQueue.getRepeatableJobs();
+    for (const jobDeBusca of cotacoesAgendadas) {
+        await cotacoesQueue.removeRepeatableByKey(jobDeBusca.key);
+    }
+
+    cotacoesQueue.add({}, 
+        {
+            repeat: { cron: '0/15 * * * *' },
+            attempts: 3,
+            backoff: 5000,
+        }
+    );
 };
 
 module.exports = {
