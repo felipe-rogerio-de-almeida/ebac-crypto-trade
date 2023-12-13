440229e5f25e5533c3531c9e9d05bdf1bb08ab39
Felipe Rogerio <felipe.rogerio@outlook.com>
2023-12-13 02:12:47 -0300

    add: configurando otp

--- services/index.js
+++ services/index.js
@@ -6,11 +6,14 @@ module.exports = {
     geraPnl     : require('./gera-pnl'),
     trocaMoedas : require('./troca-moedas'),
     confirmaConta : require('./confirma-conta'),
+    validaTokenAlteracaoDeSenha : require('./valida-token-senha'),
+    geraSegredo :require('./otp').geraSegredo,
+    validaOtp   :require('./otp').validaOtp,
     buscaCotacoesOnline: require('./busca-cotacoes').buscaCotacoesOnline,
     buscaCotacoesNoBanco: require('./busca-cotacoes').buscaCotacoesNoBanco,
     buscaVariacaoOnline: require('./calcula-variacao').buscaVariacaoOnline,
     buscaVariacoesNoBanco: require('./calcula-variacao').buscaVariacoesNoBanco,
     enviaEmailDeConfirmacao : require('./envia-email').enviaEmailDeConfirmacao,
     enviaEmailDeRecuperacao : require('./envia-email').enviaEmailDeRecuperacao,
-    validaTokenAlteracaoDeSenha : require('./valida-token-senha')
+    
 };
---
+++ services/otp.js
@@ -0,0 +1,29 @@
+const qrcode = require('qr-image');
+const { authenticator } = require('otplib')
+
+const geraSegredo = (email) =>{
+    const segredo = authenticator.generateSecret();
+
+    const otpauth = authenticator.keyuri(
+        email, 
+        'CryptoTrade',
+        segredo
+    );
+
+    const imagem = qrcode.imageSync(otpauth, {type: 'svg'} );
+
+    return {
+        segredo,
+        qrcode: imagem,
+    }
+
+}
+
+const validaOtp = (segredo, token) => {
+    return authenticator.check(tokem,segredo)
+}
+
+module.exports ={
+    geraSegredo,
+    validaOtp,
+}
