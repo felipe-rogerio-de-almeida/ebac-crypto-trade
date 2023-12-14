03153b27c6cb9edb13dbe1609a72e92e36c6e221
Felipe Rogerio <felipe.rogerio@outlook.com>
2023-12-14 17:02:17 -0300

    add: Testes de integração rota status

---
+++ tests/integration/v1/status.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+
+const app = require('../../../app');
+
+describe('/v1/status', () => {
+    test('retornar se a api está ok', () => {
+        return request(app)
+            .get('/v1/status')
+            .then(resposta =>{
+                expect(resposta.body.status).toBe('ok');
+                expect(resposta.body.sucesso).toBe(true);
+            })
+    })
+
+    test('retorna o status 200', () => {
+        return request(app)
+            .get('/v1/status')
+            .expect(200)
+    })
+})
